0e185f41225028164363d217879a38de6518f4cd
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-08-26 13:06:41 +0200

    Create solution.py

---
+++ python_essentiels1/module3/lab2/solution.py
@@ -0,0 +1,10 @@
+# Get user input
+user_input = input("Enter a string: ")
+
+# Check the input and perform conditional execution
+if user_input == "Spathiphyllum":
+    print("Yes - Spathiphyllum is the best plant ever!")
+elif user_input == "spathiphyllum":
+    print("No, I want a big Spathiphyllum!")
+else:
+    print(f"Spathiphyllum! Not {user_input}!")
