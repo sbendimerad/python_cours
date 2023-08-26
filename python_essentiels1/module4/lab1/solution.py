1855a0322bc54df9bc0440baa6584e920fa9878a
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-08-26 15:29:57 +0200

    Update solution.py

--- python_essentiels1/module4/lab1/solution.py
+++ python_essentiels1/module4/lab1/solution.py
@@ -11,16 +11,3 @@ def isYearLeap(year):
     else:
        return False
 
-
-testData = [1900, 2000, 2016, 1987]
-testResults = [False, True, True, False]
-
-
-for i in range(len(testData)):
-    yr = testData[i]
-    print(yr,"->",end="")
-    result = isYearLeap(yr)
-    if result == testResults[i]:
-        print("OK")
-    else:
-        print("Failed")
