bc349ee0d781db547d8cbfba58df270fc520ff7d
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-08-26 15:30:12 +0200

    Update solution.py

--- python_essentiels1/module4/lab2/solution.py
+++ python_essentiels1/module4/lab2/solution.py
@@ -23,17 +23,3 @@ def daysInMonth(year, month):
         return 28
         
 
-
-testYears = [1900, 2000, 2016, 1987]
-testMonths = [2, 2, 1, 11]
-testResults = [28, 29, 31, 30]
-
-for i in range(len(testYears)):
-    yr = testYears[i]
-    mo = testMonths[i]
-    print(yr, mo, "->", end="")
-    result = daysInMonth(yr, mo)
-    if result == testResults[i]:
-        print("OK")
-    else:
-        print("Failed")
