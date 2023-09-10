ff42d322f8654f580251e740783932dbe9819082
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-09-10 20:02:22 +0200

    Create solution.py

---
+++ python_essentiels2/module2/lab4/solution.py
@@ -0,0 +1,10 @@
+text = input("Enter text: ")
+
+# remove all spaces...
+text = text.replace(' ','')
+
+# ... and check if the word is equal to reversed itself
+if len(text) > 1 and text.upper() == text[::-1].upper():
+	print("It's a palindrome")
+else:
+	print("It's not a palindrome")
