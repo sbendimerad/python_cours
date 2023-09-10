e1b98e9c91ee626ec30ffc75049cd2bf7d1a2aaa
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-09-10 20:19:05 +0200

    Create solution.py

---
+++ python_essentiels2/module2/lab6/solution.py
@@ -0,0 +1,11 @@
+date = input("Enter your birthday date (in the following format: YYYYMMDD or YYYYDDMM, 8 digits): ")
+if len(date) != 8 or not date.isdigit():
+    print("Invalid date format.")
+else:
+    while len(date) > 1:
+        the_sum = 0
+        for dig in date:
+            the_sum += int(dig)
+        print(date)
+        date = str(the_sum)
+    print("Your Digit of Life is: " + date)
