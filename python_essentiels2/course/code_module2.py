c47104662211283893cd8af5d4dbb3790888b801
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-09-11 12:22:42 +0200

    Create code_module2.py

---
+++ python_essentiels2/course/code_module2.py
@@ -0,0 +1,13 @@
+# Caesar cipher.
+text = input("Enter your message: ")
+cipher = ''
+for char in text:
+    if not char.isalpha():
+        continue
+    char = char.upper()
+    code = ord(char) + 1
+    if code > ord('Z'):
+        code = ord('A')
+    cipher += chr(code)
+
+print(cipher)
