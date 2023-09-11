b84ed1a1c5088296e8ec8a4961b2ee63204114d5
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-09-11 12:32:28 +0200

    Create solution.py

---
+++ python_essentiels2/module2/lab7/solution.py
@@ -0,0 +1,16 @@
+word = input("Enter the word you wish to find: ").upper()
+strn = input("Enter the string you wish to search through: ").upper()
+
+found = True
+start = 0
+
+for ch in word:
+	pos = strn.find(ch, start) 
+	if pos < 0:
+		found = False
+		break
+	start = pos + 1
+if found:
+	print("Yes")
+else:
+	print("No")
