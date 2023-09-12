fe26e2bacc72e5477bf79c533f44ed87f667f160
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-09-12 21:35:38 +0200

    Update solution.py

--- python_essentiels1/module3/lab4/solution.py
+++ python_essentiels1/module3/lab4/solution.py
@@ -1,6 +1,5 @@
 Year = int(input("Enter a year: "))
 
-# Solution1
 if year > 1582 :
     if (year % 4) == 0:
        if (year % 100) == 0:
@@ -15,14 +14,3 @@ if year > 1582 :
 else :
     print("not within the Gregorian calendar period")"""
 
-# Solution2
-if (Year % 4) == 0:
-   if (Year % 100) == 0:
-       if (Year % 400) == 0:
-           print("{0} is a leap year".format(Year))
-       else:
-           print("{0} is not a leap year".format(Year))
-   else:
-       print("{0} is a leap year".format(Year))
-else:
-   print("{0} is not a leap year".format(Year))
