ed3f0974de9880806f66c3645b155a9c0959c0e5
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-09-12 20:55:54 +0200

    Update solution.py

--- python_essentiels1/module2/lab9/solution.py
+++ python_essentiels1/module2/lab9/solution.py
@@ -4,14 +4,26 @@ dura = int(input("Event duration (minutes): "))
 
 # put your code here
 
+## First solution
 endhour = hour + dura // 60
 endminute = mins + dura % 60
-
 
 endhour = endhour + endminute // 60
 
 endhour = endhour % 24 
 endminute = endminute % 60
 
+print(endhour,":",endminute)
+
+## Second solution 
+endhour = hour + dura // 60
+endminute = mins + dura % 60
+
+if endhour > 24 / 
+	endhour = endhour + endminute // 60
+
+if endminute > 60 :
+	endhour = endhour % 24 
+	endminute = endminute % 60
 
 print(endhour,":",endminute)
