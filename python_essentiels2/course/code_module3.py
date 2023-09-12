5cad7cd7cb7a3ac172f883a0a42080e1eca0ce94
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-09-12 16:02:05 +0200

    Update code_module3.py

--- python_essentiels2/course/code_module3.py
+++ python_essentiels2/course/code_module3.py
@@ -213,3 +213,127 @@ print(object_3.__dict__, object_3.counter)
 ## Module 3 : Méthodes
 
 #
+class MyClass:
+    def regular_method(self, parameter):
+        print("regular method", parameter)
+        
+    
+    def __init__(self, value):
+        self.initial_value = value
+        print("Constructor called with value", value)
+
+obj = MyClass(5)
+obj.regular_method("Hello")
+
+
+## Module 3 : Méthodes spéciales
+
+#
+class SuperOne:
+    pass
+
+class SuperTwo:
+    pass
+
+class Sub(SuperOne, SuperTwo):
+    pass
+
+print(Sub.__name__)
+print(Sub.__module__)
+print(Sub.__bases__)
+
+
+
+## Module 3 : Méthodes spéciales (__str__)
+
+#
+class Star:
+    def __init__(self, name, galaxy):
+        self.name = name
+        self.galaxy = galaxy
+
+sun = Star("Sun", "Milky Way")
+print(sun)
+
+class Star:
+    def __init__(self, name, galaxy):
+        self.name = name
+        self.galaxy = galaxy
+
+    def __str__(self):
+        return self.name + ' in ' + self.galaxy
+
+
+sun = Star("Sun", "Milky Way")
+print(sun)
+
+## Module 3 : L’héritage
+
+#
+class Vehicle:
+    pass
+
+class LandVehicle(Vehicle):
+    pass
+
+class TrackedVehicle(LandVehicle):
+    pass
+
+for cls1 in [Vehicle, LandVehicle, TrackedVehicle]:
+    for cls2 in [Vehicle, LandVehicle, TrackedVehicle]:
+        print(issubclass(cls1, cls2), end="\t")
+    print()
+
+## Module 3 : Gestion des propriétés et méthodes
+
+class Super:
+    def __init__(self, name):
+        self.name = name
+
+    def __str__(self):
+        return "My name is " + self.name + "."
+
+
+class Sub(Super):
+    def __init__(self, name):
+        super().__init__(name)
+        Super.__init__(self, name)
+
+obj = Sub("Andy")
+print(obj)
+
+## Module 3 : Propriétés et méthodes
+
+#
+class Level1:
+    variable_1 = 100
+    def __init__(self):
+        self.var_1 = 101
+
+    def fun_1(self):
+        return 102
+
+class Level2(Level1):
+    variable_2 = 200
+    def __init__(self):
+        super().__init__()
+        self.var_2 = 201
+    
+    def fun_2(self):
+        return 202
+
+class Level3(Level2):
+    variable_3 = 300
+    def __init__(self):
+        super().__init__()
+        self.var_3 = 301
+
+    def fun_3(self):
+        return 302
+         
+obj = Level3()
+print(obj.variable_1, obj.var_1, obj.fun_1())
+print(obj.variable_2, obj.var_2, obj.fun_2())
+print(obj.variable_3, obj.var_3, obj.fun_3())
+
+#
