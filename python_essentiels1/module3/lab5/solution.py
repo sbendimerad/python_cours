c0d04dbe4aa2fb1a3ab9c0f7f826d5b7aadac9ed
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-09-12 21:43:24 +0200

    Update solution.py

--- python_essentiels1/module3/lab5/solution.py
+++ python_essentiels1/module3/lab5/solution.py
@@ -1,10 +1,9 @@
 secret_number = 777
 
-gamer_number = int(input("enter an integer number : "))
+gamer_number = int(input("Enter an integer number: "))
 
-while gamer_number != secret_number :
-    print("Ha ha! You're stuck in my loop !")
-    gamer_number = int(input("enter an integer number : "))
+while gamer_number != secret_number:
+    print("Ha ha! You're stuck in my loop!")
+    gamer_number = int(input("Enter an integer number: "))
 
-print(gamer_number)
 print("Well done, muggle! You are free now.")
