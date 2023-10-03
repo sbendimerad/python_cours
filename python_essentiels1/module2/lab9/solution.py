6b353acc4565a33de17665968fa08df7b7bf1c0f
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-10-03 16:10:23 +0200

    Update solution.py

--- python_essentiels1/module2/lab9/solution.py
+++ python_essentiels1/module2/lab9/solution.py
@@ -14,16 +14,3 @@ endhour = endhour % 24
 endminute = endminute % 60
 
 print(endhour,":",endminute)
-
-## Second solution 
-endhour = hour + dura // 60
-endminute = mins + dura % 60
-
-if endhour > 24 / 
-	endhour = endhour + endminute // 60
-
-if endminute > 60 :
-	endhour = endhour % 24 
-	endminute = endminute % 60
-
-print(endhour,":",endminute)
