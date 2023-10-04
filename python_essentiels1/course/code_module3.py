673b539195cb7f3751de79dee7f057dab06ab9fa
Sabrine Bendimerad <sabrine.bendimerad1@gmail.com>
2023-10-04 11:44:16 +0200

    Update code_module3.py

--- python_essentiels1/course/code_module3.py
+++ python_essentiels1/course/code_module3.py
@@ -305,12 +305,23 @@ myList[1], myList[3] = myList[3], myList[1]
 
 print(myList)
 
-#Permutation complexe avec liste
+#Permutation complexe avec liste solution 1 
 myList = [10, 1, 8, 3, 5]
 length = len(myList)
 
 for i in range(length // 2):
     myList[i], myList[length - i - 1] = myList[length - i - 1], myList[i]
+
+print(myList)
+
+#Permutation complexe avec liste solution 1 
+myList = [10, 1, 8, 3, 5]
+length = len(myList)
+j = -1
+
+for i in range(length // 2):
+    myList[i], myList[j] = myList[j], myList[i]
+    j = j -1
 
 print(myList)
 
